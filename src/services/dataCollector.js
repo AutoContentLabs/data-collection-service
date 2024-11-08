2c30f0d140f18ec6e4c741965048ad41009b8079
Quenteon <quanteonquantumelysium@gmail.com>
2024-11-08 10:27:42 +0000

    test

--- src/services/dataCollector.js
+++ src/services/dataCollector.js
@@ -35,14 +35,52 @@ async function collectData(service) {
 
     // Yapılandırılmış veriyi ayıklama
     trends = jsonData.rss.channel.item.map(item => ({
+      // default info
       title: item.title,
-      approxTraffic: item["ht:approx_traffic"],
+      traffic: item["ht:approx_traffic"],
+
+      // location info
+      // region: none,
+      country_code: service.country_code, // TR
+      country_name: service.country_name, // Turkey
+      language_code: service.language_code, // TR
+      language_name: service.language_name, // Turkish
+
+      // record info
+      timestamp: new Date().toISOString(),
+
+      // report info
+      platform: service.platform_code,
+      provider: 'google_llc',
+
+      // source info
       description: item.description,
       link: item.link,
       pubDate: new Date(item.pubDate),
       picture: item["ht:picture"],
       pictureSource: item["ht:picture_source"],
-      service
+
+      // track info
+      data_schema_id: 7, // Trend Data
+      service_id: service.service_id,
+      platform_id: service.platform_id, // google_trends,
+      provider_id: 1, // google_llc,
+
+      // document info
+      data_format_id: service.data_format_id,
+
+      // cron info
+      fetch_frequency: service.fetch_frequency,
+      time_interval: service.time_interval,
+      next_fetch: service.next_fetch,
+      last_fetched: service.last_fetched,
+
+      // processing info
+      service_type_name: service.service_type_name, // API Service
+      access_method_id: service.access_method_id, // 2 : Free
+      access_type_id: service.access_type_id, // 2 : Web Rss/Xml
+      full_url: service.full_url, // service url address
+      last_error_message: service.last_error_message,
     }));
 
     // MongoDB'ye veri kaydetme
@@ -60,8 +98,12 @@ async function collectData(service) {
     // Kafka ile sinyal gönderme
     sendKafkaSignal('data-collection-complete', {
       status: 'complete',
-      source: 'Google Trends',
-      recordCount: trends.length,
+      data_schema_id: 7, // Trend Data
+      service_id: service.service_id,
+      platform_id: service.platform_id, // google_trends,
+      provider_id: 1, // google_llc,
+      records: trends,
+      record_count: trends.length,
       timestamp: new Date()
     });
 
