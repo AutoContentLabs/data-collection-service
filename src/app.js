7fa912bd040d739cfbf4adcd5de0fd512c6846a1
Quenteon <quanteonquantumelysium@gmail.com>
2024-12-03 18:20:47 +0000

    added prometheus client for metric

--- src/app.js
+++ src/app.js
@@ -8,3 +8,25 @@ const clusterManager = require('./cluster/clusterManager');
 // Start with clustering unless in production environment
 const withCluster = process.env.NODE_ENV !== "production";  // true for non-production environments
 clusterManager.start(withCluster);
+
+const http = require('http');
+const promClient = require('prom-client');
+
+// 
+const collectDefaultMetrics = promClient.collectDefaultMetrics;
+collectDefaultMetrics();
+
+// 
+const server = http.createServer((req, res) => {
+    if (req.url === '/metrics') {
+        res.setHeader('Content-Type', promClient.register.contentType);
+        res.end(promClient.register.metrics());
+    } else {
+        res.writeHead(404);
+        res.end();
+    }
+});
+
+server.listen(3000, () => {
+    console.log('Prometheus metrics listening on http://localhost:3000/metrics');
+});
